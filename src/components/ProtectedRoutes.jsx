32eadded3d5c9bdb442a85980abbfa8fc962eb58
Sai Ram Patra <saip01798@gmail.com>
2025-08-01 11:52:38 +0530

    token requirment commit

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-// import isToken from '../hooks/isToken'; 
-// import { setAuthUser } from '../redux/slices/authSlice'; 
+import isToken from '../hooks/isToken'; 
+import { setAuthUser } from '../redux/slices/authSlice'; 
 
 const ProtectedRoutes = ({ children }) => {
   const { user } = useSelector((store) => store?.auth);
   const navigate = useNavigate();
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   const check = async () => {
-  //     const res = await isToken();
-  //     if (!res?.success) {
-  //       dispatch(setAuthUser(null)); // remove user if token is bad
-  //     }
-  //   };
-  //   check();
-  // }, []);
+  useEffect(() => {
+    const check = async () => {
+      const res = await isToken();
+      if (!res?.success) {
+        dispatch(setAuthUser(null)); // remove user if token is bad
+      }
+    };
+    check();
+  }, []);
 
   useEffect(() => {
     if (!user) {
